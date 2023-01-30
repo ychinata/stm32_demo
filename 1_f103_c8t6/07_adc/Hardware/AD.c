ea0ee07a6aa690c6c7f7a0495a85db84ca19c5b6
ychinata <ychinata@qq.com>
2023-01-30 15:00:10 +0800

    No.: 1_f103_c8t6_07_adc Author:ychinata Description: 单通道ADC demo succeed (cherry picked from commit 663cffa7b1d846e733d576444b8ebfba5e6c4feb)
    No.: 1_f103_c8t6_07_adc
    Author:ychinata
    Description: 单通道ADC demo succeed-采用单次转换+非扫描模式
    (cherry picked from commit 8d0c6cefe2c16153f579c5875d328b1027848701)
    
    No.: 1_f103_c8t6_07_adc
    Author:ychinata
    Description: 多通道ADC demo succeed-采用单次转换+非扫描模式
    (cherry picked from commit 6db514e2540b8831872568dfb2e3f8bb8ebf0d64)
    
    No.: 1_f103_c8t6_07_adc
    Author:ychinata
    Description: 多通道ADC+DMA demo succeed-采用扫描/连续模式
    (cherry picked from commit ce262d0103962c11a14129ce69af10e393f19907)

---
+++ 1_f103_c8t6/07_adc/Hardware/AD.c
@@ -0,0 +1,115 @@
+#include "stm32f10x.h"                  // Device header
+
+// ADC+DMA
+uint16_t AD_Value[4];
+// ADC+DMA
+
+void AD_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	
+	RCC_ADCCLKConfig(RCC_PCLK2_Div6);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+    // 引脚配置
+    // GPIO_Pin <-> ADC_RegularChannelConfig()
+    // PA1      <-> ADC_Channel_0
+    // PA7      <-> ADC_Channel_7
+    //GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;       // 单通道    
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;       // 多通道
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	//ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5); // 对应PA0
+	//ADC_RegularChannelConfig(ADC1, ADC_Channel_7, 1, ADC_SampleTime_55Cycles5); // 对应PA7        
+	
+	ADC_InitTypeDef ADC_InitStructure;
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+	ADC_InitStructure.ADC_NbrOfChannel = 1;
+	ADC_Init(ADC1, &ADC_InitStructure);
+	
+	ADC_Cmd(ADC1, ENABLE);
+	
+	ADC_ResetCalibration(ADC1);
+	while (ADC_GetResetCalibrationStatus(ADC1) == SET);
+	ADC_StartCalibration(ADC1);
+	while (ADC_GetCalibrationStatus(ADC1) == SET);
+}
+
+// 2023.1.30
+// 简单的多通道实现：单次转换+非扫描模式
+uint16_t AD_GetValue(uint8_t adcChannelNo)
+{
+    ADC_RegularChannelConfig(ADC1, adcChannelNo, 1, ADC_SampleTime_55Cycles5);
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
+	return ADC_GetConversionValue(ADC1);
+}
+
+// ADC扫描模式+DMA转运
+void AD_DMA_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    // ADC+DMA
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	// ADC+DMA
+    
+	RCC_ADCCLKConfig(RCC_PCLK2_Div6);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+    // 填充多通道菜单
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_55Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 3, ADC_SampleTime_55Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_3, 4, ADC_SampleTime_55Cycles5);
+		
+	ADC_InitTypeDef ADC_InitStructure;
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    // ADC+DMA配置差异
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE; // DISABLE扫描模式,ENABLE连续模式.DMA配置要对应修改
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+	ADC_InitStructure.ADC_NbrOfChannel = 4;
+    // ADC+DMA配置差异
+	ADC_Init(ADC1, &ADC_InitStructure);
+	
+    // ADC+DMA
+	DMA_InitTypeDef DMA_InitStructure;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)AD_Value;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_BufferSize = 4;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular; // DMA_Mode_Normal扫描模式,DMA_Mode_Circular连续模式.ADC配置要对应修改
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
+	DMA_Init(DMA1_Channel1, &DMA_InitStructure);	
+	DMA_Cmd(DMA1_Channel1, ENABLE);    
+	ADC_DMACmd(ADC1, ENABLE);
+    // ADC+DMA
+    
+	ADC_Cmd(ADC1, ENABLE);
+	
+	ADC_ResetCalibration(ADC1);
+	while (ADC_GetResetCalibrationStatus(ADC1) == SET);
+	ADC_StartCalibration(ADC1);
+	while (ADC_GetCalibrationStatus(ADC1) == SET);
+	
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+}
---
+++ 1_f103_c8t6/07_adc/User/main.c
@@ -0,0 +1,111 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "AD.h"
+
+
+/********************引脚配置*****************
+//OLED(4针)-oled.c
+SCL-PB8
+SDA-PB9
+
+//ADC-ad.c
+input1-PA0
+input2-PA1
+input3-PA2
+input4-PA3
+**********************************************/
+
+// ADC单通道采集
+// 2023.1.30
+int main1(void)
+{
+    uint16_t ADValue;
+    float voltage;
+    
+	OLED_Init();
+	AD_Init();
+	OLED_ShowString(1, 1, "ADC Single Ch:");	
+	OLED_ShowString(2, 1, "ADValue:");
+	OLED_ShowString(3, 1, "Volatge:0.00V");
+	
+	while (1) {
+		ADValue = AD_GetValue(ADC_Channel_0);
+		voltage = (float)ADValue / 4095 * 3.3;
+		
+		OLED_ShowNum(2, 9, ADValue, 4);
+		OLED_ShowNum(3, 9, voltage, 1);     // 显示电压整数部分
+		OLED_ShowNum(3, 11, (uint16_t)(voltage * 100) % 100, 2);    // 显示电压小数部分
+		
+		Delay_ms(100);
+	}
+}
+
+
+
+// ADC多通道采集(采用单次转换+非扫描模式)
+// 2023.1.30
+int main2(void)
+{
+    uint16_t adVal0, adVal1, adVal2, adVal3; 
+    float voltage;
+    
+	OLED_Init();
+	AD_Init();
+	
+	OLED_ShowString(1, 1, "ADC Multi Ch:");    
+	OLED_ShowString(2, 1, "AD0:");
+	OLED_ShowString(3, 1, "AD1:");
+	OLED_ShowString(4, 1, "AD3:");
+    OLED_ShowChar(4, 11, '.');
+    OLED_ShowChar(4, 14, 'V');
+	//OLED_ShowString(4, 1, "AD3:");
+	
+	while (1) {
+		adVal0 = AD_GetValue(ADC_Channel_0);
+		adVal1 = AD_GetValue(ADC_Channel_1);
+		adVal2 = AD_GetValue(ADC_Channel_2);
+		adVal3 = AD_GetValue(ADC_Channel_3);
+		
+		OLED_ShowNum(2, 5, adVal0, 4);
+		//OLED_ShowNum(3, 5, adVal1, 4);
+		//OLED_ShowNum(4, 5, adVal2, 4);
+		OLED_ShowNum(4, 5, adVal3, 4);
+        
+        //显示转换成电压
+		voltage = (float)adVal3 / 4095 * 3.3;		
+		OLED_ShowNum(4, 10, voltage, 1);     // 显示电压整数部分
+		OLED_ShowNum(4, 12, (uint16_t)(voltage * 100) % 100, 2);    // 显示电压小数部分
+		
+		Delay_ms(100);
+	}
+}
+
+// ADC多通道采集+DMA
+// 2023.1.30
+int main(void)
+{
+    float voltage;    
+	OLED_Init();
+	AD_DMA_Init();
+	
+    OLED_ShowString(1, 1, "ADC+DMA MultiCh:"); 
+	OLED_ShowString(2, 1, "AD0:");
+	OLED_ShowString(3, 1, "AD1:");
+	OLED_ShowString(4, 1, "AD3:");
+    OLED_ShowChar(4, 11, '.');
+    OLED_ShowChar(4, 14, 'V');    
+	
+	while (1) {
+		OLED_ShowNum(2, 5, AD_Value[0], 4);
+		OLED_ShowNum(4, 5, AD_Value[3], 4);
+        
+        //显示转换成电压
+		voltage = (float)AD_Value[3] / 4095 * 3.3;		
+		OLED_ShowNum(4, 10, voltage, 1);     // 显示电压整数部分
+		OLED_ShowNum(4, 12, (uint16_t)(voltage * 100) % 100, 2);    // 显示电压小数部分        
+		
+		Delay_ms(100);
+	}
+}
+
