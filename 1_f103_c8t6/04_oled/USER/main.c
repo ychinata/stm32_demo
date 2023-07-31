ae083e4cf5eb6c3742b52623d94c57a43dab39aa
ychinata <ychinata@qq.com>
2023-07-31 16:57:49 +0800

    No.: vl53l0x_demo_f103c8t6 Author:ychinata Description:STP-23L跑通，显示在OLED上

--- 1_f103_c8t6/04_oled/USER/main.c
+++ 1_f103_c8t6/04_oled/USER/main.c
@@ -15,7 +15,7 @@ int main(void)
     OLED_ShowString(1, 1, "Hello,4Pin OLED!");
     OLED_ShowString(3, 1, "2022.5.1");    
 	// OLED_ShowString(1, 3, "HelloWorld!");
-	// OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowNum(2, 1, 12345, 5);
 	// OLED_ShowSignedNum(2, 7, -66, 2);
 	// OLED_ShowHexNum(3, 1, 0xAA55, 4);
 	// OLED_ShowBinNum(4, 1, 0xAA55, 16);
--- 1_f103_c8t6/15_laser_range/3_stp-23L/USER/main.c
+++ 1_f103_c8t6/15_laser_range/3_stp-23L/USER/main.c
@@ -1,5 +1,12 @@
 /*************************************
 使用串口3接收STP-23L模块数据，使用串口1输出接收成功的次数，oled显示距离
+
+LDROBOT正面摆放，由上至下的四根线依次为：
+4.P5V 绿
+3.GND 黑
+2.RX  红
+1.TX  黄
+
 接线说明：
 STP-23L模块      单片机接口
 -----------------------------------
@@ -25,17 +32,19 @@ int main(void)
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置中断优先级分组，即优先级分级个数
 	                                                //NVIC_PriorityGroup_2，代表抢占优先级位数位2，可以分[0, 1, 2, 3]四级优先级
 	                                                //                          响应优先级位数位2，可以分[0, 1, 2, 3]四级优先级
-	delay_init();//延时初始化
-	
+	delay_init();//延时初始化	
 	uart_init(115200);//串口1初始化，用于输出接收数据帧成功的次数
 	/*关闭JTAG接口*/
 	JTAG_Set(JTAG_SWD_DISABLE);    
-
 	/*打开SWD接口 可以利用主板的SWD接口调试*/
 	JTAG_Set(SWD_ENABLE);           
-
 	OLED_Init();
-	uart3_init(230400);//雷达串口接收
+	uart3_init(230400);//激光雷达串口接收
+    
+    OLED_ShowString(1, 1, "STP-23L Laser");
+            OLED_ShowString(3, 7, "mm");
+    OLED_ShowString(4, 1, "2023.7.31");
+    
 	while(1)
 	{
 		printf("成功接收%d次\r\n",receive_cnt);
@@ -48,7 +57,7 @@ int main(void)
 		delay_ms(10);
 		//oled显示
 		OLED_SHOW();
-		delay_ms(10);
+		delay_ms(1000);
 		
 	}
 }
@@ -56,18 +65,20 @@ int main(void)
 /*  oled显示  */
 void OLED_SHOW(void) 
 {
-		OLED_ShowString(0,0,"Distance:");			/* 显示距离 */
-		OLED_ShowString(110,0,"mm"),
-		OLED_ShowNumber(68,0,distance,5,12);
-		OLED_ShowString(0,10,"noise:");				/* 显示环境噪音 */
-		OLED_ShowNumber(68,10,noise,5,12);
-		OLED_ShowString(0,20,"peak:");				/* 显示接收强度信息 */
-		OLED_ShowNumber(68,20,peak,5,12);
-		OLED_ShowString(0,30,"con:");				  /* 置信度 */
-		OLED_ShowNumber(68,30,confidence,5,12);
-		OLED_ShowString(0,40,"intg:");				/* 积分次数 */
-		OLED_ShowNumber(68,40,intg,5,12);
-		OLED_ShowString(0,50,"reftof:");			/* 温度表征值 */
-		OLED_ShowNumber(68,50,reftof,5,12);
-		OLED_Refresh_Gram();	
+		OLED_ShowString(2, 1, "Distance:");			/* 显示距离 */
+        OLED_ShowNum(3, 1, distance, 5);    
+//		OLED_ShowString(0,0,"Distance:");			/* 显示距离 */
+//		OLED_ShowString(110,0,"mm"),
+//		OLED_ShowNumber(68,0,distance,5,12);
+//		OLED_ShowString(0,10,"noise:");				/* 显示环境噪音 */
+//		OLED_ShowNumber(68,10,noise,5,12);
+//		OLED_ShowString(0,20,"peak:");				/* 显示接收强度信息 */
+//		OLED_ShowNumber(68,20,peak,5,12);
+//		OLED_ShowString(0,30,"con:");				  /* 置信度 */
+//		OLED_ShowNumber(68,30,confidence,5,12);
+//		OLED_ShowString(0,40,"intg:");				/* 积分次数 */
+//		OLED_ShowNumber(68,40,intg,5,12);
+//		OLED_ShowString(0,50,"reftof:");			/* 温度表征值 */
+//		OLED_ShowNumber(68,50,reftof,5,12);
+//		OLED_Refresh_Gram();	
 }
