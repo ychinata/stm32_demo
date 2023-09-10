31a84764ac51d78f312a4bdcd1b57631ca297f82
ychinata <ychinata@qq.com>
2023-09-10 17:10:44 +0800

    No.: 1_f103c8_16_decoder Author:xy Description: 教程版左右轮编码器实现成功：TIM4对应PB7/6，TIM2对应PA1/0 问题： 左轮-正向数值为几十，反向数值为67000（大于最大值65535） 右轮-正向数值一千多，反向数值可小于65535，转得慢也是大于65535

--- 1_f103_c8t6/16_encoder/HARDWARE/ENCODER.h
+++ 1_f103_c8t6/16_encoder/HARDWARE/ENCODER.h
@@ -16,14 +16,30 @@ B相 <-> PA0
 GND
 M+
  */
- //TIM4
-#define ENCODER_PORT    GPIOB
-#define ENCODER_RCC     RCC_APB2Periph_GPIOB
-#define ENCODER_PIN     GPIO_Pin_7 | GPIO_Pin_6
+//左轮 TIM2/PA1/PA0
+#define ENCODERL_TIM    TIM2
+#define ENCODERL_TIM_RCC    RCC_APB1Periph_TIM2
+#define ENCODERL_PORT   GPIOA
+#define ENCODERL_RCC    RCC_APB2Periph_GPIOA
+#define ENCODERL_PIN    GPIO_Pin_1 | GPIO_Pin_0
 
+//右轮 TIM4/PB7/PB6
+#define ENCODERR_TIM    TIM4
+#define ENCODERR_TIM_RCC    RCC_APB1Periph_TIM4
+#define ENCODERR_PORT   GPIOB
+#define ENCODERR_RCC    RCC_APB2Periph_GPIOB
+#define ENCODERR_PIN    GPIO_Pin_7 | GPIO_Pin_6
+/********************************************
+    编码器引脚定义
+ *********************************************/
+ 
+#define ENCODER_TIM_PERIOD (u16)(65535)   //不可大于65535 因为F103的定时器是16位的。
+
+#define ENCODER_ID_LEFT 0
+#define ENCODER_ID_RIGHT 1
 
 // 函数声明
-void Encoder_Init_TIM4(u16 arr,u16 psc);
-int Read_Encoder_TIM4(void);
+void ENCODER_Init(void);
+int ENCODER_ReadCnt(int encoderId);
 
 #endif
--- 1_f103_c8t6/16_encoder/USER/main.c
+++ 1_f103_c8t6/16_encoder/USER/main.c
@@ -7,16 +7,17 @@ int main(void)
 {
 	delay_init();
 	uart_init(9600);
-	Encoder_Init_TIM4(0xffff,0);
+	ENCODER_Init();
     OLED_Init();
     
-	OLED_ShowString(1, 1, "Encoder:");
-	
+	OLED_ShowString(1, 1, "Encoder Left:");
+	OLED_ShowString(3, 1, "Encoder Right:");
     
     while(1) {		 
 		delay_ms(200); //每隔200ms读取一次编码器计数，即速度。
 		               //可以使用定时中断实现更精准的速度计算，用户可自定义
-        printf("Encoder=%d\r\n", Read_Encoder_TIM4());
-        OLED_ShowNum(2, 1, Read_Encoder_TIM4(), 5);
+        printf("Encoder=%d\r\n", ENCODER_ReadCnt(ENCODER_ID_LEFT));
+        OLED_ShowNum(2, 1, ENCODER_ReadCnt(ENCODER_ID_LEFT), 5);
+        OLED_ShowNum(4, 1, ENCODER_ReadCnt(ENCODER_ID_RIGHT), 5);
 	}
 }
